4eb9681556ff4cde1d9fbf1a15e49a2829878795
Tomasz <twlodarczyk@arizona.edu>
2023-06-08 14:40:55 -0700

    Add new

--- Solitude_Correlation.R
+++ Solitude_Correlation.R
@@ -1983,5 +1983,132 @@ rmse
   
 }
 
+#NEW DATASET REMOVING NDs (hisstogram)
+{
+setwd("C:/Users/twlodarczyk/OneDrive - University of Arizona/Desktop/All documents/1 PhD/CNRS + Synch/Field/Soltitude/Data/Solitude New")
+dt <- read.delim("Solitude_Complete_List_6.2.23_Final.txt")
+dt <- subset(dt, Scientific_Name != 'QA_Sample')
 
 
+#removing LODs
+{
+  tr <- matrix(data = NA, ncol = ncol(dt[,c(1:48)]), nrow=nrow(dt)) # select all columns 1:46
+  colnames(tr) <- colnames(dt[,c(1:48)])
+  for (i in 14:48) # select when the concentrations start
+  {
+    tr[,c(i)] <- gsub(".*ND.*", 0, dt[,i])
+  }
+  
+  for(i in 1:13) # select columns that need to stay the same 1:11 include character and double (weight)
+  {
+    tr[,c(i)] <- dt[,c(i)]
+  }
+  tr   
+  
+  #transform to dataframe
+  tr <- as.data.frame.matrix(tr) #A correct command to change the dataset to dataframe after transformations
+  tr[,14:48] <- sapply(tr[,14:48],as.numeric) # Change a character to numeric (double)
+  typeof(tr$Cu_concentration) # confirm the value is no longer a character
+}
+#apply new LODs
+{
+  tr$Cl_concentration[tr$Cl_concentration == 0] <- 50/2
+  tr$Ca_concentration[tr$Ca_concentration == 0] <- 10/2
+  tr$Ti_concentration[tr$Ti_concentration == 0] <- 5/2
+  tr$Cr_concentration[tr$Cr_concentration == 0] <- 2/2
+  tr$Mn_concentration[tr$Mn_concentration == 0] <- 1/2
+  tr$Fe_concentration[tr$Fe_concentration == 0] <- 5/2
+  tr$Co_concentration[tr$Co_concentration == 0] <- 3/2
+  tr$Ni_concentration[tr$Ni_concentration == 0] <- 0.2/2
+  tr$Cu_concentration[tr$Cu_concentration == 0] <- 0.5/2
+  tr$Zn_concentration[tr$Zn_concentration == 0] <- 0.6/2
+  tr$As_concentration[tr$As_concentration == 0] <- 0.1/2
+  tr$Se_concentration[tr$Se_concentration == 0] <- 0.1/2
+  tr$Cd_concentration[tr$Cd_concentration == 0] <- 1/2
+  tr$Re_concentration[tr$Re_concentration == 0] <- 0.5/2
+  tr$Hg_concentration[tr$Hg_concentration == 0] <- 0.3/2
+  tr$Tl_concentration[tr$Tl_concentration == 0] <- 1/2
+  tr$Pb_concentration[tr$Pb_concentration == 0] <- 0.2/2
+}
+
+#write.table(tr, file='C:/Users/twlodarczyk/OneDrive - University of Arizona/Desktop/All documents/1 PhD/CNRS + Synch/Field/Soltitude/Data/Solitude New/SLT_pXRF_Final_06.06.23.csv', sep=",", row.names = F)
+dt <- read.delim("SLT_pXRF_Final_06.06.23.txt")
+dt <- subset(dt, Site=="TAILINGS")
+
+dt <- dt[dt$Cu_concentration != 0.25, ] # To remove LODs
+hist(dt$Cu_concentration, breaks=50)
+
+
+colors <- c(P1 = "#0070C0", P2 = "#92D050", P5 = "#EDAD08", P6 = "#ED7D31")
+
+# Create the violin plot
+Cu <- ggplot(dt, aes(x = Plot, y = Cu_concentration, fill=Plot)) +
+  geom_violin() +
+  geom_point(position = position_jitter(width = 0.1), size = 2, alpha = 0.5) +
+  labs(x = "Plot", y = "Cu Concentration", fill = "Plot") +
+  geom_hline(yintercept = 300, linetype = "dotdash", color = "#454545", size = 1.2) +
+  coord_flip() +
+  scale_fill_manual(values = colors) +
+  scale_y_continuous(limits = c(0, 600), breaks = seq(0, 600, by = 100)) +
+  theme_minimal()+
+  theme(
+        axis.text.x = element_text(  size=14),
+        axis.title.x = element_text(size = 19),
+        axis.text.y = element_text(size=14, face="italic"),
+        axis.title.y = element_blank(),
+        legend.key.size = unit(1, "lines"),
+        legend.text = element_text(size = 13.5), 
+        legend.title = element_text(size=15, face = "bold"))+
+  guides(color = guide_legend(override.aes = list(size = 3.5)),
+         shape = guide_legend(override.aes = list(size = 3.5))) 
+Cu
+
+Se <- ggplot(dt, aes(x = Plot, y = Se_concentration, fill=Plot)) +
+  geom_violin() +
+  geom_point(position = position_jitter(width = 0.1), size = 2, alpha = 0.5) +
+  labs(x = "Plot", y = "Se Concentration", fill = "Plot") +
+  #geom_hline(yintercept = 300, linetype = "dotdash", color = "#454545", size = 1.2) +
+  coord_flip() +
+  scale_fill_manual(values = colors) +
+  scale_y_continuous(limits = c(0, 60), breaks = seq(0, 60, by = 20)) +
+  theme_minimal()+
+  theme(
+    axis.text.x = element_text(size=14),
+    axis.title.x = element_text(size = 19),
+    axis.text.y = element_text(size=14, face="italic"),
+    axis.title.y = element_blank(),
+    legend.key.size = unit(1, "lines"),
+    legend.text = element_text(size = 13.5), 
+    legend.title = element_text(size=15, face = "bold"))+
+  guides(color = guide_legend(override.aes = list(size = 3.5)),
+         shape = guide_legend(override.aes = list(size = 3.5))) 
+Se
+
+Re <- ggplot(dt, aes(x = Plot, y = Re_concentration, fill=Plot)) +
+  geom_violin() +
+  geom_point(position = position_jitter(width = 0.1), size = 2, alpha = 0.5) +
+  labs(x = "Plot", y = "Re Concentration", fill = "Plot") +
+  #geom_hline(yintercept = 300, linetype = "dotdash", color = "#454545", size = 1.2) +
+  coord_flip() +
+  scale_fill_manual(values = colors) +
+  #scale_y_continuous(limits = c(0, 60), breaks = seq(0, 60, by = 20)) +
+  theme_minimal()+
+  theme(
+    axis.text.x = element_text(size=14),
+    axis.title.x = element_text(size = 19),
+    axis.text.y = element_text(size=14, face="italic"),
+    axis.title.y = element_blank(),
+    legend.key.size = unit(1, "lines"),
+    legend.text = element_text(size = 13.5), 
+    legend.title = element_text(size=15, face = "bold"))+
+  guides(color = guide_legend(override.aes = list(size = 3.5)),
+         shape = guide_legend(override.aes = list(size = 3.5))) 
+
+Re
+
+library(ggpubr)
+
+ggarrange(Cu, Se, Re, ncol = 1, nrow = 3, 
+          common.legend = TRUE, legend = "bottom")
+
+}
